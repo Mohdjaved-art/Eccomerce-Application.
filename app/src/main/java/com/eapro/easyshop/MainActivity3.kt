f9038f9f3618dd186f4481e5c6e39a4bf64b3216
Mohdjaved-art <devtest1209@gmail.com>
2024-07-04 12:15:03 +0530

    "this is from javedEapro"

---
+++ app/src/main/java/com/eapro/easyshop/MainActivity3.kt
@@ -0,0 +1,18 @@
+package com.eapro.easyshop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.eapro.easyshop.ui.main.MainFragment
+
+class MainActivity3 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main3)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
